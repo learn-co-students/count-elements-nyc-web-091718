18ee2f8dbf364e6f8936f28503f693de8fb243a6
Max Petersen <mglxp5@gmail.com>
2018-09-17 21:38:11 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,14 @@
 def count_elements(array)
-  # code goes here
+  animals_hash = {}
+  animal_names = array.uniq
+  animal_names.each do |animal|
+    count = 0
+    array.each do |element|
+      if animal == element
+        count += 1
+      end
+    end
+    animals_hash[animal] = count
+  end
+  animals_hash
 end
- 
