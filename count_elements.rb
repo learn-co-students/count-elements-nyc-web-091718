2b113de3f940b1de2322d5ec42720b08f1bb88cb
kristopherbullinger <github email address>
2018-09-18 17:42:51 -0400

    Lab complete

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  hash = {}
+  array.each do |element_1|
+    if !hash[element_1]
+      hash[element_1] = 0
+      array.each do |element_2|
+        hash[element_1] += 1 if element_1 == element_2
+      end
+    end
+  end
+  hash
 end
- 
